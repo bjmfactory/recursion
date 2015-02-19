d0d4d82d6462fbe579ce892d669fc914767e2b9a
Ben Johnson <ben@bjmfactory.com>
2015-02-19 17:56:50 -0600

    first stab at recursive but it's not quite right

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,15 +5,31 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-
   var results = [];
   var body = document.body;
-
-  var hasClass = function(element, className){
-    return element.classList.contains(className);
+  var element = body;
+  var children = element.childNodes;
+  var hasClass = function(element){
+    if (element.classList){
+      return element.classList.contains(className);
+    }
+    else {
+      return false;
+    }
   };
 
-  hasChildNodes() // returns boolean
+  for(var i = 0; i < children.length; i++){
+    if (hasClass(children[i])){
+      results.push(children[i]);
+    }
+    if (children[i].hasChildNodes()){
+      element = children[i];
+      // getElementsByClassName(className); This is where I would expect to call function
+      // within itself, but it give me a stack overflow.
+    }
+  }
+
+  return results;
 };
 
 /* algorithm
