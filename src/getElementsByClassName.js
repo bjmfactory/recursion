6ed3503313b9693a70d152d1f9184016bedcee61
Ben Johnson <ben@bjmfactory.com>
2015-02-19 17:08:03 -0600

    first steps for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+
+  var results = [];
+  var body = document.body;
+
+  var hasClass = function(element, className){
+    return element.classList.contains(className);
+  };
+
+  hasChildNodes() // returns boolean
 };
+
+/* algorithm
+
+base case is no more children.
+
+loop through children of document.body
+  if a child has the class, add it to the results array
+  check to see if that child element has children
+
+loop through all those children
+  if a child has the class, add it to the results array
+  check to see if that child element has children
+
+*/
