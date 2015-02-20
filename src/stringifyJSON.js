c018c172df33666c632f3332299e0fec74b1e24f
Ben Johnson <ben@bjmfactory.com>
2015-02-20 11:17:01 -0600

    going through different types of objects and stringifying them: number, null, boolean, string, and good progress on array.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,22 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  var newString = '';
-  for (var key in obj){
-    newString += key;
-    newString += obj[key];
+  var results = '';
+
+  if (typeof obj === "number"){
+    results = obj.toString();
+  };
+
+  if (obj === null){
+    results = 'null';
   }
 
-  newString = '"{' + newString + '}"';
-  console.log(newString);
-  return newString;
+  if (typeof obj === "boolean"){
+    results = obj.toString();
+  }
+
+  if (typeof obj === "string"){
+    results = results + '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)){
+    results = '['
+    for (var i = 0; i < obj.length; i++){
+      results = results + stringifyJSON(obj[i]) + ',';
+    }
+    if (results.length > 1){
+      results = results.substring(0, results.length - 1)
+    }
+    results += ']';
+  }
+
+  return results;
 };
-
-var makeString = function(object) {
-  if (typeof object === string){
-    object = '"' + object + '"';
-  }
-  else {
-    object = object
-  }
-}
